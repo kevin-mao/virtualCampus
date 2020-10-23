b1c278a914591d13896abada26c0d0f3ffc2c660
Ainur Kaishibayeva <ainur@Ainurs-MBP.lan>
2020-10-22 20:47:02 -0400

    New spaces page

---
+++ src/pages/spaces.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import GridItem from "../components/material-kit-components/Grid/GridItem.js";
+// import GridContainer from "../components/material-kit-components/Grid/GridContainer.js";
+import {Template, Title} from "../components";
+
+export default function Spaces() {
+    return (
+        <Template title={'Spaces'}>
+        <div style={{textAlign:'center', width:'60%', marginLeft:'auto', marginRight:'auto', marginTop:'5%'}}>
+              <Title>Virtual Spaces</Title>
+        </div>
+            
+
+            
+        </Template>
+    );
+}
